a724fc0797395df7d55a059d08901a05aa78b545
michelle23-code <michellelekoto13@icloud.com>
2024-12-14 17:41:07 +0200

    updated APP.jsx
    added react icons

--- virtual-Assistant/src/App.jsx
+++ virtual-Assistant/src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import "./App.css"
+import va from "./assets/ai.png"
+import { CiMicrophoneOn } from "react-icons/ci";
 function App() {
   return (
-    <div>App</div>
-  )
+    <div className="main">
+      <img src={va} alt="" id="shifra" />
+      <span>I'm Shifra,Your Advanced virtual Assistant</span>
+      <button>
+        Click here
+        <CiMicrophoneOn />
+      </button>
+    </div>
+  );
 }
 
 export default App
